8f4999894aca5e9cef0b84e966d5d0fbc9d3a7d4
tana2013 <ammari2019pro@gmail.com>
2020-02-29 21:35:45 +0300

    the local server saved file

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ if (process.env.NODE_ENV !== 'production'){
 const express = require ('express')
 const app = express()
 const expressLayouts = require ('express-ejs-layouts')
+const bodyParser = require ('body-parser')
 
 const router = require ('./routes/api')
 const AuthorsRouter = require ('./routes/authors')
-const bodyParser = require ('body-parser')
+const BooksRouter = require ('./routes/books')
 
 
 app.set('view engine','ejs')
@@ -21,7 +22,7 @@ app.use(bodyParser.urlencoded({ limit:'10mb',extended: false}))
 const mongoose = require ('mongoose')
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true, useUnifiedTopology: true})
 const db = mongoose.connection
-db.on('error', error => console.error(error));
+db.on('error', error => console.error(error))
 db.once('open',() => console.log('Connected to Mongoose! ^^'))
 
 
@@ -29,7 +30,7 @@ db.once('open',() => console.log('Connected to Mongoose! ^^'))
 
 app.use('/', router)
 app.use('/authors', AuthorsRouter)
-
+app.use('/books', BooksRouter)
 
 app.listen(process.env.PORT || 1100)
 
--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,15 @@
 const express = require ('express')
 const router = express.Router()
+const Book = require ('../models/book')
 
-router.get('/',(req,res)=>{
-    res.render('index')
-
+router.get('/', async (req,res) => {
+    let books
+    try {
+        books = await  Book.find().sort({ createdAt:'desc'}).limit(10).exec()
+    } catch {
+        books =[]
+    }
+    res.render('index',{ books: books})
 })
-router.get('/', function(req, res){
-    res.redirect('index');
- })
 
 module.exports = router
